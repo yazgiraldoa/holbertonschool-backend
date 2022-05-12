fcc2e09b67b39c82098d5c6ab53d9b2fa86ea228
yazgiraldoa <edygiraldo.97@gmail.com>
2022-05-11 22:37:17 -0500

    add LRU and MRU caching system

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+LIFO caching
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    LIFO caching class
+    """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """Method to save data in cache"""
+        if key and item:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                item = list(self.cache_data.keys())[BaseCaching.MAX_ITEMS - 1]
+                self.cache_data.pop(item)
+                print('DISCARD: {}'.format(item))
+
+    def get(self, key):
+        """Method to get a key from cache"""
+        return self.cache_data.get(key)
